61c8b9161b47d1083f345d1b0b9fb5c42f2c967a
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-03-22 14:44:04 -0700

    added constructor to pull in data from api and started setting state. also imported bootstrap form to display lat and lon

--- src/app.js
+++ src/app.js
@@ -2,15 +2,45 @@ import React from 'react';
 import './app.css';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: {},
+      locationSearch: ''
+    }
+  }
 
-render(){
-  return(
-    <h1>city explorer</h1>
-  );
-}
+  getLocationData = async () => {
+    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_KEY}&q=${this.state.locationSearch}&format=json`;
+    const location = await axios.get(url);
+    const locationArray = location.data;
+    this.setState({
+      location: locationArray[0]
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <h1>City Explorer</h1>
+        <Form>
+          <Form.Group>
+            <Form.Label>City</Form.Label>
+            <Form.Control type="text" placeholder="Enter city" />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Explore!
+          </Button>
+        </Form>
+
+      </>
+    );
+  }
 }
 
 
